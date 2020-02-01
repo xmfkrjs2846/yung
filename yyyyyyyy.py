8445e46cecbc3c0099798f9003a03a3ea4fac3b1
xmfkrjs2846 <54546949+xmfkrjs2846@users.noreply.github.com>
2020-02-01 18:14:58 +0900

    Update yyyyyyyy.py

--- yyyyyyyy.py
+++ yyyyyyyy.py
@@ -38,14 +38,14 @@ async def on_message(message):
    if message.content.startswith("퇴근"):
          await client.send_message(message.channel, "수고하셨어요~! 저 먼저 퇴근할게요!\n(앞으로 융이 아무 말도 하지 않습니다)")
          del talk[0]
-         talk.append(0)
+         talk.append(1)
          await client.change_presence(status=discord.Status.idle, game=discord.Game(name="집에서 노는 것을", type=1))
    if message.content.startswith("출근"):
          await client.send_message(message.channel, "오늘도 야근인가... 후우...;;")
          del talk[0]
-         talk.append(1)
+         talk.append(0)
          await client.change_presence(game=discord.Game(name="버그시 호출;;| help", type=1))
-   if talk[0] == 1:
+   if talk[0] == 0:
     if message.content.startswith("융"):
         await client.send_message(message.channel,"융융!")
     if message.content.startswith("혹시"):
@@ -198,7 +198,7 @@ async def on_message(message):
         me = await client.get_user_info('356311092332593152')
         msg = "{0.author.mention}이가 부르고있어 어서 대답해주는게 좋을 것 같은데?".format(message)
         await client.send_message(me, "{}".format(msg))
-        await client.send_message(message.channel, "{} 260만원 쓴 음머어~ 찾아요!!".format(myid))
+        await client.send_message(message.channel, "{} 375만원 쓴 음머어~ 찾아요!!".format(myid))
     if message.content.startswith("안녕"):
         if message.content[0:] == '안녕':
              msg = "반가워~ {0.author.mention}아!!".format(message)
