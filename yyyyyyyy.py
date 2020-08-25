fa036ab9de128db83994d714af1ee96f3d35e992
xmfkrjs2846 <54546949+xmfkrjs2846@users.noreply.github.com>
2020-08-26 02:32:11 +0900

    Update yyyyyyyy.py

--- yyyyyyyy.py
+++ yyyyyyyy.py
@@ -619,7 +619,7 @@ async def on_message(message):
                 embed.add_field(name='출근', value='융이 출근하여 다시 말을 할 수 있습니다', inline=False)
                 embed.add_field(name='퇴근', value='융이 퇴근하여 사내가 조용합니다.', inline=False)
                 await message.channel.send(embed=embed)
-    else:
+    if message.author = client.user:
         if message.content.startswtih("융"):
             await message.channel.send("응 나 불렀어?")
             await message.channel.send("아니면 말고..")
